2d178cfb8ad923659a1e03c20224dcea4701744c
Rajguru Jenish <rajgurujenish>
2025-02-23 12:07:26 +0530

    Modify templates to display dara from database

--- chart/views.py
+++ chart/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Chart
 
 # Create your views here.
 def chart_list(request):
-    return render(request, 'chart/chart_list.html',{})
+    charts = Chart.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'chart/chart_list.html',{'charts': charts})

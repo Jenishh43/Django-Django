4ed983d23238b5946121bed6a8142dc6521ad77b
Rajguru Jenish <rajgurujenish>
2025-02-23 17:14:51 +0530

    add templates

--- chart/urls.py
+++ chart/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns =[
     path('', views.chart_list, name='chart_list'),
+    path('chart/<int:pk>/', views.chart_list, name='chart_detail'),
 ]
--- chart/views.py
+++ chart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Chart
 
@@ -6,3 +6,7 @@ from .models import Chart
 def chart_list(request):
     charts = Chart.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'chart/chart_list.html',{'charts': charts})
+
+def chart_detail(request, pk):
+    chart = get_object_or_404(Chart, pk=pk)
+    return render(request, 'chart/chart_detail.html', {'chart': chart})
